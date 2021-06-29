0483f9181044f33d3d77859d7fe667c325c14dc0
StevenPlai <79199940+StevenPlai@users.noreply.github.com>
2021-06-29 04:42:59 -0500

    Update Offer Map.R

--- Offer Map.R
+++ Offer Map.R
@@ -8,13 +8,22 @@ library(extrafont)
 library(lubridate)
 library(gridExtra)
 library(egg)
+library(maptools)
+library(rgdal)
+library(rtweet)
+library(png)
+library(ggpubr)
 
-font_import()  
+na_cities <- data.frame()
+
+for(i in 2022:2023)
+{ 
+cycle <- i
 
 x <- getURL("https://raw.githubusercontent.com/StevenPlai/SoonerReport/main/uscities.csv")
 city_data <- read.csv(text = x)
 
-offers <- read_html("https://247sports.com/Season/2024-Football/Offers?ViewPath=~/Views/SkyNet/RecruitInterest/_SimpleDetailedSetForSeason.ascx&skey=12")
+offers <- read_html(paste0("https://247sports.com/Season/",cycle,"-Football/Offers?ViewPath=~/Views/SkyNet/RecruitInterest/_SimpleDetailedSetForSeason.ascx&skey=12"))
 
 locations <- offers %>% html_nodes(".meta") %>% html_text() %>% 
   trimws() %>% str_extract_all("(?<=\\().+?(?=\\))")
@@ -36,9 +45,11 @@ location_data <- locations %>% separate(col = city, sep = ",", into = c("city", 
 location_data$state_id <- trimws(location_data$state_id)
 
 geo_data <- left_join(location_data, city_data, by=c("city", "state_id")) 
+n_offers <- nrow(geo_data)
+na_cities <- bind_rows(na_cities, geo_data %>% filter(is.na(lng)))
 geo_data <- geo_data %>% group_by(city, state_id) %>% summarise(lat = first(lat), 
-                                                lng = first(lng),
-                                                offers = n()) %>% ungroup() %>%
+                                                                lng = first(lng),
+                                                                offers = n()) %>% ungroup() %>%
   filter(!is.na(lng))
 
 coords <- geo_data %>% select(lng,lat)
@@ -52,30 +63,47 @@ now <- format(as.Date(now),'%m/%d/%Y')
 
 p <- plot_usmap() +
   geom_point(data = geo_data, aes(x=lng, y=lat, size = offers), 
-            alpha = .35, color = "red") +
-  scale_size_continuous(range = c(4, 1.25*max(geo_data$offers)),
+             alpha = .35, color = "red") +
+  scale_size_continuous(range = c(4, 1.5*max(geo_data$offers)),
                         name = "Offers", breaks = c(sort(unique(geo_data$offers)))) +
   labs(title = "Cumulative Offer Map",
-       subtitle = "Oklahoma Class of 2019",
-       caption = paste0("@SoonerReport\nUpdated Through ",now)) +
+       subtitle = paste0("Oklahoma Class of ",cycle),
+       caption = paste0("Data: 247Sports, USCities\nAs of ",now,"\n@SoonerReport")) +
   theme_fivethirtyeight() +
+  annotation_custom(text_grob(paste0("Total Offers = ",n_offers),
+                              hjust = -.8, vjust = -1, face = "plain",
+                              family = "Arial", size = 20), xmin = .9, xmax = 1, ymin = .5) +
   theme(
-    plot.title = element_text(size = 45, hjust = .5, family = ".SF NS Rounded", vjust = .7),
-    plot.subtitle = element_text(size = 30, hjust = .5, family = ".SF NS Rounded"),
-    plot.caption = element_text(size=15, face="bold", family = "Courier New", hjust = 1),
+    plot.title = element_text(size = 45, hjust = .5, family = "Arial", face = "bold", vjust = .7),
+    plot.subtitle = element_text(size = 30, hjust = .5, family = "Arial", face = "plain"),
+    plot.caption = element_text(size=15, family = "Courier New", face = "bold", hjust = 1),
     legend.position = c(.65,.06),
-    legend.title = element_text(family = ".SF NS Rounded", size = 15),
+    legend.title = element_text(family = "Arial", face = "plain", size = 15),
     panel.grid.major = element_blank(), 
     panel.grid.minor = element_blank(),
     axis.text.x = element_blank(),
     axis.text.y = element_blank(),
+    plot.background = element_rect(fill="white"),
+    panel.background = element_rect(fill="white"),
+    legend.background = element_rect(fill="white"),
+    legend.key = element_rect(fill = NA)
+    
   )
 
 grid.arrange(egg::set_panel_size(p=p, height=unit(7, "in"), 
                                  width=unit(10, "in")))
 
-ggsave("/Users/stevenplaisance/desktop/CumeOfferMap24.png", 
+ggsave(paste0("~/desktop/Offers/CumeOfferMap",cycle,".png"), 
        plot = egg::set_panel_size(p=p, width=unit(10, "in"), height=unit(7, "in")),
        height = 9, width = 10.35)
 
+}
 
+post_tweet(
+  status = "Updated #Sooners Cumulative Offer Maps",
+  token = token,
+  media = c("~/desktop/Offers/CumeOfferMap2022.png","~/desktop/Offers/CumeOfferMap2023.png"))
+
+prev_nas <- read.csv("~/desktop/Offers/na_cities.csv")
+na_cities <- bind_rows(prev_nas, na_cities)
+write.csv(na_cities, "~/desktop/Offers/na_cities.csv")
