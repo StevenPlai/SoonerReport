acc448efbb73fac2b372fcd3703df3141e866479
StevenPlai <79199940+StevenPlai@users.noreply.github.com>
2021-07-07 21:00:06 -0500

    Added Rivals Forecast Scraper Function
    Modified code from Akshay Easwaran

---
+++ RF Scraper v2.R
@@ -0,0 +1,225 @@
+library(rvest)
+library(tidyverse)
+library(lubridate)
+library(glue)
+library(httr)
+library(jsonlite)
+library(stringr)
+library(logging)
+
+loginfo("Starting Rivals FutureCast scraping...")
+
+trim <- function (x) gsub("^\\s+|\\s+$", "", gsub("^\\t|\\t$", "", x))
+column <- function(x, css) x %>% html_node(css = css) %>% html_text()
+
+fixHeight <- function(x) paste0(floor(x/12),"-",x-(floor(x/12)*12))
+
+splitJoin <- function(x) strsplit(x, split="\\s") %>% trim() %>% .[lapply(., length) > 0] %>% unlist() %>% paste()
+
+rf_html <- read_html("https://n.rivals.com/futurecast")
+
+# ----- FutureCast Forecasters -----
+loginfo("Looking for Forecaster information...")
+forecast_nodes <- rf_html %>%
+  html_nodes("[class^=\"ForecasterThumbnail_forecasterInformation__\"]")
+
+loginfo(glue("Found {length(forecast_nodes)} forecaster nodes. Parsing..."))
+forecasters <- data.frame(
+  title = column(forecast_nodes, "[class^=\"ForecasterThumbnail_forecasterTitle__\"]"),
+  first_name = column(forecast_nodes, "[class^=\"Link_link__1xDdm ForecasterThumbnail_forecasterName__\"] > div:nth_child(1)"),
+  last_name = column(forecast_nodes, "[class^=\"Link_link__1xDdm ForecasterThumbnail_forecasterName__\"] > div:nth_child(2)"),
+  accuracy = column(forecast_nodes, "[class^=\"ForecasterThumbnail_forecasterAccuracy__\"]"),
+  stringsAsFactors = FALSE
+)
+
+loginfo(glue("Parsed {nrow(forecasters)} forecaster rows. Cleaning..."))
+forecasters <- forecasters %>%
+  mutate(
+    title = trim(title),
+    first_name = trim(first_name),
+    last_name = trim(last_name),
+    full_name = glue("{first_name} {last_name}"),
+    accuracy = as.numeric(gsub("% accuracy", "", accuracy))
+  ) %>%
+  select(full_name, title, accuracy)
+
+loginfo(glue("Found/parsed/cleaned {nrow(forecasters)} forecaster rows"))
+
+# ----- FutureCasts -----
+loginfo(glue("Looking for recruit futurecast nodes..."))
+
+fc_nodes <- rf_html %>%
+  html_nodes("[class^=\"ForecastActivity_forecastActivity__\"] > [class^=\"ForecastActivity_activityText__\"]")
+
+loginfo(glue("Found {length(fc_nodes)} recruit futurecast nodes, parsing..."))
+futurecasts <- data.frame(
+  forecaster = column(fc_nodes, "[class^=\"ForecastActivity_forecastText__\"] > b:nth-child(1)"),
+  recruit = column(fc_nodes, "[class^=\"ForecastActivity_forecastText__\"] > b:nth_child(2)"),
+  profile_url = fc_nodes %>% html_node("[class^=\"ForecastActivity_forecastText__\"] > b:nth_child(2) > a") %>% html_attr('href'),
+  full_text = column(fc_nodes, "[class^=\"ForecastActivity_forecastText__\"]"),
+  time_since = column(fc_nodes, "[class^=\"ForecastActivity_forecastTime__\"]"),
+  stringsAsFactors = FALSE
+)
+
+loginfo(glue("Found {nrow(futurecasts)} recruit futurecast rows,."))
+loginfo(glue("Cleaning and filtering based on criteria: school ({selected_school}) and year ({target_year})"))
+
+now <- now(tz = "UTC")
+futurecasts <- futurecasts %>%
+  mutate(
+    forecaster = trim(forecaster),
+    recruit = trim(recruit),
+    time_since = gsub(" ago", "", time_since),
+    player_id = as.numeric(sub(".*-", "", profile_url)),
+    year = str_extract(full_text, "\\((\\d{4}),"),
+    year = sub("\\D","", year),
+    year = sub(",","", year),
+    year = as.integer(year),
+    forecasted_team = str_extract(full_text, "to (.*)\\."),
+    forecasted_team = sub("to ","", forecasted_team),
+    forecasted_team = sub("\\.","", forecasted_team),
+    update = if_else(grepl("updates", full_text, fixed = T), 1, 0),
+    original_school = if_else(update == 0, "NA", str_extract(full_text, "(?<=from\\s)\\w+"))) %>%
+  filter(selected_school == forecasted_team | selected_school == original_school)
+
+futurecasts <- left_join(futurecasts, forecasters, by = c("forecaster"="full_name"))
+
+new_futurecasts <- futurecasts %>% filter(selected_school == forecasted_team)
+
+changed_futurecasts <- futurecasts %>% filter(selected_school == original_school)
+
+loginfo(glue("Found {nrow(new_futurecasts)} New FutureCasts and {nrow(changed_futurecasts)} Changed FutureCasts"))
+
+strip_suffix <- function(name) {
+  name <- trim(name)
+  result <- case_when(
+    grepl(' Jr$', name) ~ gsub("Jr","", name),
+    grepl(' Jr\\.$', name) ~ gsub("Jr.","", name),
+    grepl(' II$', name) ~ gsub("II","", name),
+    grepl(' III$', name) ~ gsub("III","", name),
+    grepl(' IV$', name) ~ gsub("IV","", name),
+    grepl(' V$', name) ~ gsub("V","", name),
+    grepl(' VI$', name) ~ gsub("VI","", name),
+    grepl(' VII$', name) ~ gsub("VII","", name),
+    TRUE ~ trim(name)
+  )
+  return(result)
+}
+
+query_croots <- function(name, year) {
+  body <- paste0("{
+        \"search\": {
+            \"member\": \"Prospect\",
+            \"query\": \",",strip_suffix(name),",\",
+            \"sport\": \"Football\",
+            \"page_number\": \"1\",
+            \"page_size\": \"50\",
+            \"recruit_year\": \"",year,"\"
+        }
+    }")
+  croot_req <- POST("https://n.rivals.com/api/v1/people", add_headers(
+    "User-Agent"="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0",
+    Accept="application/json, text/plain, */*",
+    "Accept-Language"="en-US,en;q=0.5",
+    Referer="https://n.rivals.com/search",
+    "Content-Type"="application/json;charset=utf-8",
+    "X-XSRF-TOKEN"="JWSTp21Yj1wpNjaVz7W7x/wNgbUCNxLTFP0oHbOtYWUXdXMt2hvX+NQ3ejC8as5FAb1RE/KwiH3bY4SlsEl+Sg==",
+    "Origin"="https://n.rivals.com",
+    "Connection"="keep-alive",
+    "Cookie"="A1=d=AQABBL_NHmACEKNsUb5rZatSKxxQNn_WTEQFEgEBAQEfIGAoYAAAAAAA_SMAAA&S=AQAAAkKziEj7gaF2mOKKRDUJWqg; A3=d=AQABBL_NHmACEKNsUb5rZatSKxxQNn_WTEQFEgEBAQEfIGAoYAAAAAAA_SMAAA&S=AQAAAkKziEj7gaF2mOKKRDUJWqg; A1S=d=AQABBL_NHmACEKNsUb5rZatSKxxQNn_WTEQFEgEBAQEfIGAoYAAAAAAA_SMAAA&S=AQAAAkKziEj7gaF2mOKKRDUJWqg; GUC=AQEBAQFgIB9gKEId1wSk; XSRF-TOKEN=JWSTp21Yj1wpNjaVz7W7x%2FwNgbUCNxLTFP0oHbOtYWUXdXMt2hvX%2BNQ3ejC8as5FAb1RE%2FKwiH3bY4SlsEl%2BSg%3D%3D; _rivalry_session_v2=ZWZZQVZSQVh3ZnNaSWxaZlgwNU95aXV3a3VFREM2SE9iME9wZG4yaERvd0hHNjJOekhGZG1BOHo4WDM5OFdCSnFCczRWQ0hKelRzMWJna1NDZXV3MndjNHhaZlU1S0ZEdmJFaTVUR1doQ1RHdUJhRlQxUis2cWRBblUyVlhKcXVnVHE0aFlDODZscVMvQWxwRWxQUmpwUGk4QWdXRWUwdTlIZ2lxUk5YdzlJa3RxVVdac3lBdGRiQkxBOEErYXl2djNGT1dXa1g0c3ozSHJJdDZyMkNFZmd0eDZhUmE5TVFHU2RLc045RHlqMFFjUjJvbzVVOU9MNG9ORU9qdHY4K21wTjdtVlNVVU1KWTFNWlY0MUp6eDY3cnNZMHdNYVg5aGQ5TUZoM2doZnZEK1NvWkRkNjBiVVFlYmlyTzVqb21WVFlRRXlmWEVZSmtHZTVsc0tENkdBPT0tLVNNQmpEbzFNMGI1blVuKzVyR1Q3SFE9PQ%3D%3D--0947bcffa7fd9b9703f5522cb9afa8fac7d83254; GUCS=Ae_tyY5k; ywandp=10002066977754%3A1333922239; _cb_ls=1; _ga=GA1.2.770716812.1612631490; _gid=GA1.2.1018170693.1612631490; _gat=1; A1=d=AQABBL_NHmACEKNsUb5rZatSKxxQNn_WTEQFEgEBAQEfIGAoYAAAAAAA_SMAAA&S=AQAAAkKziEj7gaF2mOKKRDUJWqg; A1S=d=AQABBL_NHmACEKNsUb5rZatSKxxQNn_WTEQFEgEBAQEfIGAoYAAAAAAA_SMAAA&S=AQAAAkKziEj7gaF2mOKKRDUJWqg; A3=d=AQABBL_NHmACEKNsUb5rZatSKxxQNn_WTEQFEgEBAQEfIGAoYAAAAAAA_SMAAA&S=AQAAAkKziEj7gaF2mOKKRDUJWqg; GUC=AQEBAQFgIB9gKEId1wSk; _rivalry_session_v2=QW9Cckk0MGF3bnZtSFBLMFVhNlp3M281bFkzajM0dW9KWjh5clJEUWNNVytqU0w5dDg4OGJFY0NiMDNLZ0N1b3g2NEhWcVNvWm40QVdDNmlROGozMXJGazdMQmsrWUo0ZitnZkhHSFcweERuQkwxNUdQdmFESng0bTdPZlJPcTlhYy94QndBNlNWdERSMzRMVWtSbHFxT1l1VlB3eC9TbGN0OFJId2R0S1AwRE16R09GcDArbDVSUVFWcldxQzFoekJibkxNWjVQOVlRZS9vZVBkcTVpWkFqYUFIRkhLMVluaktxdllUNDRUWGw2SjdteDR3K2VHbmk0TytGWFZHdzVJaUNhanN3OFNlbDlxVGMxVEFNeXUrWFVORmt2N0ZkYnVBRUVEaEtJRTBjbHUzSUxiemRiZU8wN3hqZGJDcFA4Nnk3eDJHOGVRVzdmb2pRYUNNNDJnPT0tLXJ4c1ZUUjFXY0dqUHltWE1sSitjUHc9PQ%3D%3D--ab9e9cd73c80953acce2a86a86eb53ab3b0bfabf",
+    "TE"="Trailers"
+  ), body = body, encode = "json")
+  
+  result <- content(croot_req, "text")
+  result <- fromJSON(result)$people
+  return(result)
+}
+
+get_croot_info <- function(name, player_id, year) {
+  result <- query_croots(name, year)
+  result <- result %>%
+    filter(
+      as.numeric(prospect_id) == as.numeric(player_id)
+    ) %>%
+    head(1)
+  return(result)
+}
+
+expanded_data <- data.frame()
+player_slim_list <- new_futurecasts %>%
+  select(recruit, player_id, year) %>% group_by(player_id, year) %>% 
+  summarise(recruit = first(recruit))
+total <- nrow(player_slim_list)
+
+if (total > 0) {
+  loginfo(glue("Retrieving expanded info from Rivals API for {nrow(new_futurecasts)} New FutureCasts"))
+  for (row in 1:total) {
+    player_id <- player_slim_list[row, "player_id"]
+    name  <- trim(player_slim_list[row, "recruit"])
+    year  <- player_slim_list[row, "year"]
+    
+    
+    tryCatch(
+      {
+        loginfo(glue("Starting loading {row}/{total}: {name} (ID: {player_id}, Year: {year})"))
+        result <- get_croot_info(name, player_id, year)
+        expanded_data <- rbind(expanded_data, result)
+      },
+      error = function(cond) {
+        logwarn(paste("Error: ", cond))
+      },
+      finally = {
+        loginfo(glue("Done loading {row}/{total}: {name} (ID: {player_id}, Year: {year})"))
+      }
+    )
+  }
+  
+  
+  new_futurecasts <- left_join(new_futurecasts, expanded_data, by = c("player_id" = "prospect_id", "year" = "year"))
+  loginfo(glue("Found and joined expanded info for {nrow(futurecasts)} FutureCasts"))
+  
+  loginfo(glue("Tweeting New FutureCasts"))
+  
+  for (row in 1:total) {
+    link <- new_futurecasts[row, "profile_url"]
+    
+    player_profile <- read_html(link) 
+    
+    player_hs <- player_profile %>% html_nodes("div.new-prospect-profile > 
+                                                    div.prospect-personal-information > 
+                                                    div.location-block > 
+                                                    div.right-personal-information > 
+                                                    a > .prospect-small-information > 
+                                                    .vital-line-location") %>%
+      html_text()
+    
+    name  <- trim(new_futurecasts[row, "recruit"])
+    year  <- new_futurecasts[row, "year"]
+    pos <- new_futurecasts[row, "position_abbreviation"]
+    rank <- new_futurecasts[row, "stars"]
+    ht <- fixHeight(new_futurecasts[row, "height"])
+    wt <- new_futurecasts[row, "weight"]
+    predictor <- new_futurecasts[row, "forecaster"]
+    acc <- new_futurecasts[row, "accuracy"]
+    hs <- player_hs[2]
+    hometown <- new_futurecasts[row, "hometown"]
+    
+    text <-  glue(
+      "
+            \U0001F52E New #Sooners FutureCast
+            
+            {year} {rank}-Star {pos} {name}
+            {ht} / {wt}
+            {hs} ({hometown})
+            
+            By: {predictor} ({acc} in 2022)
+            
+            {link}
+            ")
+    loginfo(glue("Tweet {row}/{total} posted"))
+  }
+  } else {
+  loginfo(glue("Did not have any FutureCasts to find expanded info for, returning empty dataframe"))
+  futurecasts <- data.frame()
+}
+
