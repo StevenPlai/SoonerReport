ff0009058803f8c05358dc0b53e4616ba09bdec0
StevenPlai <79199940+StevenPlai@users.noreply.github.com>
2021-07-07 21:00:26 -0500

    Delete RF Scraper.R

--- RF Scraper.R
+++
@@ -1,9 +0,0 @@
-library(rvest)
-library(tidyverse)
-library(rtweet)
-library(lubridate)
-library(glue)
-
-rf <- read_html("https://n.rivals.com/futurecast") %>% 
-  html_nodes(".ForecastActivity_forecastTime__2ECSU") %>% html_text()
-
