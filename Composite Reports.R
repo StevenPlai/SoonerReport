5025586237959d169d2e95ce530e8f8d8e487616
StevenPlai <stevenrplai@gmail.com>
2021-08-04 15:00:22 -0500

    Update Composite Reports.R

--- Composite Reports.R
+++ Composite Reports.R
@@ -1,19 +1,68 @@
-library(tidyverse)
-library(ggplot2)
-library(stringi)
-library(lubridate)
-library(ggthemes)
-library(zoo)
-library(png)
-library(ggimage)
-library(grid)
+library(tidyverse, warn.conflicts = F)
+library(ggplot2, warn.conflicts = F)
+library(stringi, warn.conflicts = F)
+library(lubridate, warn.conflicts = F)
+library(ggthemes, warn.conflicts = F)
+library(zoo, warn.conflicts = F)
+library(png, warn.conflicts = F)
+library(ggimage, warn.conflicts = F)
+library(gridExtra, warn.conflicts = F)
+library(grid, warn.conflicts = F)
+library(rtweet, warn.conflicts = F)
+library(glue, warn.conflicts = F)
+library(ggtext, warn.conflicts = F)
+library(magick, warn.conflicts = F)
+library(extrafont, warn.conflicts = F)
 
-comp <- read.csv("~/desktop/Composite Scrapes/RunningCompositeRankings2022.csv")
+rcycle <- function() {
+  date <- Sys.Date()
+  year <- as.numeric(date %>% stri_sub(1,4))
+  month <- as.numeric(date %>% stri_sub(6,7))
+  mday <- as.numeric(date %>% stri_sub(9,10))
+  wday <- wday(date)
+  if (month>2) {as.character(year+1)
+  } else {
+    if (month==1) {as.character(year)} else {
+      if(mday>7) {as.character(year+1)} else{
+        if(mday==1) {as.character(year)} else {
+          if(mday==2) {
+            if(wday==5){as.character(year+1)
+            } else{as.character(year)}} else {
+              if(mday==3) {
+                if(wday==5|wday==6){as.character(year+1)
+                } else{as.character(year)}} else{
+                  if(mday==4) {
+                    if(wday>4){as.character(year+1)
+                    } else{as.character(year)}} else{
+                      if(mday==5) {
+                        if(wday>4|wday==1){as.character(year+1)
+                        } else{as.character(year)}} else{
+                          if(mday==6) {
+                            if(wday==3|wday==4){as.character(year)
+                            } else{as.character(year+1)}} else{
+                              if(mday==7) {
+                                if(wday==4){as.character(year)
+                                } else{as.character(year+1)}}}}}}}}}}}}
 
-teams <- sort(c("Oklahoma", "Alabama", "Ohio State", "Clemson", "Georgia"))
-colors <- c("#9E1B32", "#F56600", "#BA0C2F", "#BB0000", "#841617")
+cycle <- rcycle()
+
+token <- create_token(
+  app = "SoonerBot",
+  "cdJCktf6KzIPpYjSoiCO6cWjX",
+  "24UlgdHgMiwNDmKOHFVv4AyOgo8ZjQnWF0fViGrrFcbycW5yVa",
+  access_token = "1295762265493450753-y16u6RLNSKk89d6mhaUUQQPU0FAbvt",
+  access_secret = "jfTvU0gdAF4rSLJA3VtIvWlMNuOkOfipFs5uXCPUyhSEO",
+  set_renv = F
+)
+
+ttf_import(paths = "~/desktop/SR Fonts")
+
+comp <- read.csv(glue("~/desktop/Composite Scrapes/RunningCompositeRankings{cycle}.csv"))
+
+teams <- sort(c("Oklahoma", "Alabama", "Ohio State", "Clemson", "Georgia", "Texas"))
+colors <- c("#9E1B32", "#F56600", "#BA0C2F", "#BB0000", "#841617", "#BF5700")
 logos <- "NULL"
-for(x in 1:5){
+for(x in 1:6){
   x <- teams[x]
   n <- paste0("~/desktop/Logos/",x,".png")
   logos <- c(logos,n)
@@ -26,26 +75,25 @@ report <- comp %>% filter(team %in% teams) %>% group_by(team) %>%
   mutate(rmean = rollmean(rating, 3, align = "right", fill = 0)) %>% ungroup()
 report$time <- ymd_hms(report$time, tz = "America/Chicago")
 window <- Sys.time()
-window <- window-weeks(3)
+window <- window-days(30)
 report <- report %>% mutate(test = time-window) %>% filter(test>0) %>% select(-test) 
-initials <- as_vector(report %>% slice(1:5) %>% arrange(team) %>% select(rating))
+initials <- as_vector(report %>% slice(1:6) %>% arrange(team) %>% select(rating))
 
-
-ggplot(data = report, aes(x = time, y=rating)) +
+p <- ggplot(data = report, aes(x = time, y=rating)) +
   scale_color_manual(values = colors) +
   geom_line(aes(color = team), size = 4) +
-  labs(title = "Three-Week Performance Report",
-       subtitle = "Class of 2022",
-       caption = paste0("Data: 247Sports\n@SoonerReport"),
+  labs(title = "30-Day Performance Report",
+       subtitle = glue("Class of {cycle}"),
+       caption = "Data: 247Sports\n@SoonerReport",
        y = "247 Composite Rating",
        x = "Date") +
   theme_fivethirtyeight() +
   theme(
-    plot.title = element_text(size = 45, hjust = .5, family = "Arial", face = "bold", vjust = .7),
-    plot.subtitle = element_text(size = 30, hjust = .5, family = "Arial", face = "plain"),
+    plot.title = element_text(size = 45, hjust = .5, family = "SF Pro Display Regular", face = "plain", vjust = .7),
+    plot.subtitle = element_text(size = 30, hjust = .5, family = "SF Pro Display Light", face = "plain"),
     plot.caption = element_text(size=15, family = "Courier New", face = "bold", hjust = 1),
-    axis.title = element_text(size = 18, family = "Arial", face = "bold"),
-    axis.text = element_text(size = 15, family = "Arial"),
+    axis.title = element_text(size = 18, family = "SF Pro Display Light", color = "black"),
+    axis.text = element_text(size = 15, family = "SF Pro Display Light"),
     plot.background = element_rect(fill="white"),
     panel.background = element_rect(fill="white"),
     legend.position = "none") +
@@ -58,8 +106,25 @@ ggplot(data = report, aes(x = time, y=rating)) +
   annotation_custom(rasterGrob(readPNG(logos[4])), 
                     xmax=min(report$time), ymin=initials["rating4"]-4, ymax =initials["rating4"]+4) +
   annotation_custom(rasterGrob(readPNG(logos[5])), 
-                    xmax=min(report$time), ymin=initials["rating5"]-4, ymax =initials["rating5"]+4)
-  
+                    xmax=min(report$time), ymin=initials["rating5"]-4, ymax =initials["rating5"]+4) +
+  annotation_custom(rasterGrob(readPNG(logos[6])), 
+                    xmax=min(report$time), ymin=initials["rating6"]-4, ymax =initials["rating6"]+4)
 
+ggsave(glue("~/desktop/CompositeReports/30DayReport{cycle}.png"), 
+       plot = egg::set_panel_size(p=p, width=unit(10, "in"), height=unit(7, "in")),
+       height = 9.5, width = 11)
+
+p <- image_read(glue("~/desktop/CompositeReports/30DayReport{cycle}.png"))
+l <- image_read("~/desktop/LOGO.png")
+i <- image_composite(p,image_scale(l,"550"),offset="+5+2530")
+
+image_write(i, path = glue("~/desktop/CompositeReports/30DayReport{cycle}.png"), format = "png")
+
+post_tweet(status = "#Sooners 30-Day Performance Report",
+           media = glue("~/desktop/CompositeReports/30DayReport{cycle}.png"),
+           token = token)
+
+  
+rasterGrob(readPNG("~/desktop/LOGO.png"))
 
 
